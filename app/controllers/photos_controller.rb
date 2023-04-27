ca27d6f418a0a4ac071a8e0bce255b09e1bf9b46
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-27 16:20:03 +0000

    built out photo loop

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,8 @@
 class PhotosController < ApplicationController
 
   def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:created_at => :asc})
     render({:template => "photo_templates/index.html.erb"})
   end
 
