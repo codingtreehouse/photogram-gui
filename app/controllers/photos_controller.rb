0512657494cb917fc6f953ba02e4634c531222ef
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-27 16:28:43 +0000

    update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
 
   def index
     matching_photos = Photo.all
-    @list_of_photos = matching_photos.order({:created_at => :asc})
+    @list_of_photos = matching_photos.order({:created_at => :desc})
     render({:template => "photo_templates/index.html.erb"})
   end
 
